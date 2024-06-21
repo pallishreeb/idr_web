55b89a132fe7777a1d7fe478223b1dac12f71281
Pallishree <pallishreebehera01@gmail.com>
2024-06-21 18:38:08 +0530

    minor changes

--- src/page/Idr_workorder/WorkOrder.jsx
+++ src/page/Idr_workorder/WorkOrder.jsx
@@ -59,7 +59,7 @@ const WorkOrder = () => {
     const day = String(d.getDate()).padStart(2, '0');
     const month = String(d.getMonth() + 1).padStart(2, '0'); // Months are 0-based
     const year = d.getFullYear();
-    return `${day}-${month}-${year}`;
+    return `${month}-${day}-${year}`;
   }
   
   return (
