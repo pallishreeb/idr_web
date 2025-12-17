3dfbfe041b0c3dbc2f0de582975ebaa729a3a49d
Pallishree <pallishreebehera01@gmail.com>
2025-12-17 21:44:20 +0530

    new chnages in navbar

--- src/Components/AdminSideNavbar.jsx
+++ src/Components/AdminSideNavbar.jsx
@@ -33,7 +33,7 @@ const AdminSideNavbar = () => {
   ];
 
   const idrMenuItems = [
-        { title: "Client Employees", path: "/client-employees", roles: ["Client Employee"] },
+    { title: "Client Employees", path: "/client-employees", roles: client_type === "Admin" ? ["Client Employee"] : [""], },
     { title: "IDR Employees", path: "/idr-employees", roles: ["Admin", "Subadmin"] },
     { title: "Service Agreements", path: "/service-agreements", roles: client_type !== "User" 
       ? ["Client Employee"] 
